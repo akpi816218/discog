c924e219e1c5594b25161acd6173d30e872e93bc
akpi816218 <111009970+akpi816218@users.noreply.github.com>
2022-10-07 11:04:20 -0700

    Created cheesetouch command (#2)

---
+++ commands/cheesetouch.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder, userMention } = require('discord.js');
+const { readFileSync, writeFileSync, write } = require('node:fs');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('name')
+		.setDescription('description')
+		.addUserOption((option) => {
+			return option
+				.setName('user')
+				.setDescription('The user to give the cheese touch to')
+				.setRequired(true);
+		}),
+	execute: async (interaction) => {
+		if (interaction.user.id == readFileSync(__dirname + '/cheesetouch.txt')) {
+			writeFileSync(
+				__dirname + '/cheesetouch.txt',
+				interaction.options.getUser('user').id
+			);
+			await interaction.reply(
+				`${userMention(
+					interaction.user.id
+				)} gave the cheese touch to ${userMention(
+					interaction.options.getUser('user').id
+				)}.`
+			);
+		} else {
+			await interaction.reply(
+				`${userMention(
+					readFileSync(__dirname + '/cheesetouch.txt')
+				)} has the cheese touch, not you.`
+			);
+		}
+	},
+};
