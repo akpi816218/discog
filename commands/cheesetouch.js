0c2efdbf74f41c7dc31aeca83130e5461688acc9
akpi816218 <111009970+akpi816218@users.noreply.github.com>
2022-10-08 12:49:49 -0700

    forceful cheese infection (#8)

--- commands/cheesetouch.js
+++ commands/cheesetouch.js
@@ -1,5 +1,5 @@
-const { SlashCommandBuilder, userMention } = require('discord.js');
-const { readFileSync, writeFileSync, write } = require('node:fs');
+const { SlashCommandBuilder, userMention, bold } = require('discord.js');
+const { readFileSync, writeFileSync } = require('node:fs');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,25 +10,53 @@ module.exports = {
 				.setName('user')
 				.setDescription('The user to give the cheese touch to')
 				.setRequired(true);
+		})
+		.addBooleanOption((option) => {
+			return option
+				.setName('force')
+				.setDescription("This won't work unless you own the bot")
+				.setRequired(false);
 		}),
 	execute: async (interaction) => {
-		if (interaction.user.id == readFileSync(__dirname + '/cheesetouch.txt')) {
+		if (!interaction.options.getBoolean('force', false)) {
+			if (interaction.user.id == readFileSync(__dirname + '/cheesetouch.txt')) {
+				writeFileSync(
+					__dirname + '/cheesetouch.txt',
+					interaction.options.getUser('user').id
+				);
+				await interaction.reply(
+					`${userMention(
+						interaction.user.id
+					)} gave the cheese touch to ${userMention(
+						interaction.options.getUser('user').id
+					)}.`
+				);
+			} else {
+				await interaction.reply(
+					`${userMention(
+						readFileSync(__dirname + '/cheesetouch.txt')
+					)} has the cheese touch, not you.`
+				);
+			}
+		} else if (
+			![
+				1006248060629811301 /* akpi */, 817214551740776479 /* akhilzebra */,
+			].includes(interaction.user.id)
+		) {
 			writeFileSync(
 				__dirname + '/cheesetouch.txt',
 				interaction.options.getUser('user').id
 			);
 			await interaction.reply(
-				`${userMention(
-					interaction.user.id
+				`${userMention(interaction.user.id)} ${bold(
+					'forcefully'
 				)} gave the cheese touch to ${userMention(
 					interaction.options.getUser('user').id
 				)}.`
 			);
 		} else {
 			await interaction.reply(
-				`${userMention(
-					readFileSync(__dirname + '/cheesetouch.txt')
-				)} has the cheese touch, not you.`
+				'You do not have the permission to use this command in this way.'
 			);
 		}
 	},
