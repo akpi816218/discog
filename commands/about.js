ae79ec1ea78b09b2c72b456d75e7c8e5efb278a0
Akhl Pillai <816218@seq.org>
2022-10-12 13:31:21 -0700

    added about and help

--- commands/about.js
+++ commands/about.js
@@ -22,7 +22,9 @@ module.exports = {
 						text: 'About DisCog',
 						iconURL:
 							'https://raw.githubusercontent.com/akpi816218/discog/gitmaster/discog.png',
-					}),
+					})
+					.setTitle('About DisCog')
+					.setDescription(`DisCog is a versatile general purpose Discord bot. DisCog features utility commands as well as some games.`),
 			],
 		});
 	},
--- commands/z.template.ts
+++ commands/z.template.ts
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+// ! When creating a new command, be sure to add it to `coghelp.js` in both the general embed and specific switch statement.
+
 module.exports = {
 	data: new SlashCommandBuilder().setName('name').setDescription('description'),
 	execute: async (interaction) => {
