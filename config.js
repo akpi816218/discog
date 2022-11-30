4dc665508ab4e88ae7a564418226896a0831dbd5
Akhil Pillai <816218@seq.org>
2022-11-29 19:15:08 -0800

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-export const applicationId = '1034561538721325056';
-export const clientId = '1034561538721325056';
-export const inviteLink =
-	'https://discord.com/api/oauth2/authorize?client_id=1034561538721325056&permissions=2147732480&scope=bot%20applications.commands';
