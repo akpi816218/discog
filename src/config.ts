c214cce1ce2a1747a49a8b8bcce7cd72d451d147
Akhil Pillai <816218@seq.org>
2023-06-23 12:44:50 -0700

    QR command

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,7 @@
 import { Snowflake } from 'discord.js';
-export const applicationId: Snowflake = '1027383984675639348';
+// % Production
+// export const applicationId: Snowflake = '1027383984675639348';
+// % Development
+export const applicationId: Snowflake = '1034561538721325056';
 export const clientId = applicationId;
 export const inviteLink = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&permissions=1644906413303&scope=bot%20applications.commands`;
