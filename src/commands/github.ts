ef5c4f69e11aa815f3753481146fa7ad054dacee
akpi816218 <816218@seq.org>
2023-06-24 08:05:41 -0700

    save

--- src/commands/github.ts
+++ src/commands/github.ts
@@ -1,4 +1,7 @@
 import {
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
 	ChatInputCommandInteraction,
 	EmbedBuilder,
 	SlashCommandBuilder
@@ -6,15 +9,53 @@ import {
 import { Octokit } from 'octokit';
 
 export const data = new SlashCommandBuilder()
-	.setName('name')
-	.setDescription('description');
+	.setName('github')
+	.setDescription('Get information about a GitHub user.')
+	.addSubcommand((subcommand) => {
+		return subcommand
+			.setName('profile')
+			.setDescription("View a GitHub user's profile.")
+			.addStringOption((option) => {
+				return option
+					.setName('username')
+					.setDescription('The GitHub username to view.')
+					.setRequired(true);
+			});
+	})
+	.addSubcommand((subcommand) => {
+		return subcommand
+			.setName('repos')
+			.setDescription("View a GitHub user's repositories.")
+			.addStringOption((option) => {
+				return option
+					.setName('username')
+					.setDescription('The GitHub username to view.')
+					.setRequired(true);
+			});
+	})
+	.addSubcommand((subcommand) => {
+		return subcommand
+			.setName('stats')
+			.setDescription("View a GitHub user's stats.")
+			.addStringOption((option) => {
+				return option
+					.setName('username')
+					.setDescription('The GitHub username to view.')
+					.setRequired(true);
+			});
+	});
 
 // ! Make sure to add command to `coghelp.ts`
 
-const BaseEmbed = new EmbedBuilder()
-		.setColor('White')
-		.setDescription('Fetching data from GitHub, please wait...')
-		.setTimestamp(),
+const BaseEmbed = (interaction: ChatInputCommandInteraction) =>
+		new EmbedBuilder()
+			.setColor('White')
+			.setDescription('Fetching data from GitHub, please wait...')
+			.setTimestamp()
+			.setFooter({
+				iconURL: interaction.client.user.displayAvatarURL(),
+				text: 'Powered by DisCog'
+			}),
 	OctoKit = new Octokit({ userAgent: 'DisCog' });
 const Handlers: {
 	// eslint-disable-next-line no-unused-vars
@@ -22,7 +63,7 @@ const Handlers: {
 } = {
 	profile: async (interaction: ChatInputCommandInteraction) => {
 		await interaction.reply({
-			embeds: [BaseEmbed.setTitle('Github | Profile')]
+			embeds: [BaseEmbed(interaction).setTitle('Github | Profile')]
 		});
 		const { data } = await OctoKit.rest.users.getByUsername({
 			username: interaction.options.getString('username', true)
@@ -30,8 +71,30 @@ const Handlers: {
 		if (!data.id)
 			await interaction.editReply({
 				embeds: [
-					BaseEmbed.setDescription(null)
+					BaseEmbed(interaction)
+						.setTitle('GitHub | Profile')
+						.setDescription('This user does not exist on GitHub.')
+				]
+			});
+		else
+			await interaction.editReply({
+				components: [
+					new ActionRowBuilder<ButtonBuilder>().setComponents(
+						new ButtonBuilder()
+							.setStyle(ButtonStyle.Link)
+							.setLabel('View full profile on GitHub')
+							.setURL(data.html_url)
+					)
+				],
+				embeds: [
+					BaseEmbed(interaction)
+						.setTitle('GitHub | Profile')
+						.setDescription(null)
 						.setFields(
+							{
+								name: 'Username',
+								value: interaction.options.getString('username', true)
+							},
 							{
 								name: 'Name',
 								value: data.name ?? 'Not Provided'
@@ -57,9 +120,110 @@ const Handlers: {
 				]
 			});
 	},
-	repos: async (interaction: ChatInputCommandInteraction) => {},
-	stats: async (interaction: ChatInputCommandInteraction) => {},
-	timeline: async (interaction: ChatInputCommandInteraction) => {}
+	repos: async (interaction: ChatInputCommandInteraction) => {
+		await interaction.reply({
+			embeds: [BaseEmbed(interaction).setTitle('Github | Repositories')]
+		});
+		const username = interaction.options.getString('username', true);
+		const { data } = await OctoKit.rest.repos.listForUser({
+			username: username
+		});
+		if (!data.length)
+			await interaction.editReply({
+				embeds: [
+					BaseEmbed(interaction).setDescription(
+						'This user either has no public repositories or does not exist.'
+					)
+				]
+			});
+		else
+			await interaction.editReply({
+				components: [
+					new ActionRowBuilder<ButtonBuilder>().setComponents(
+						new ButtonBuilder()
+							.setStyle(ButtonStyle.Link)
+							.setLabel('View full list on GitHub')
+							.setURL(`https://github.com/${username}`)
+					)
+				],
+				embeds: [
+					BaseEmbed(interaction)
+						.setDescription(null)
+						.setFields(
+							...data.map((repo) => ({
+								name: repo.name,
+								value:
+									repo.description ??
+									`No description provided.` +
+										`\n${repo.html_url}\n${repo.stargazers_count ?? 0} Stars`
+							}))
+						)
+				]
+			});
+	},
+	stats: async (interaction: ChatInputCommandInteraction) => {
+		await interaction.reply({
+			embeds: [BaseEmbed(interaction).setTitle('Github | Stats')]
+		});
+		const { data } = await OctoKit.rest.users.getByUsername({
+			username: interaction.options.getString('username', true)
+		});
+		if (!data.id)
+			await interaction.editReply({
+				embeds: [
+					BaseEmbed(interaction).setDescription(
+						'This user does not exist on GitHub.'
+					)
+				]
+			});
+		else
+			await interaction.editReply({
+				components: [
+					new ActionRowBuilder<ButtonBuilder>().setComponents(
+						new ButtonBuilder()
+							.setStyle(ButtonStyle.Link)
+							.setLabel('View full profile on GitHub')
+							.setURL(data.html_url)
+					)
+				],
+				embeds: [
+					BaseEmbed(interaction)
+						.setTitle('GitHub | Stats')
+						.setDescription(null)
+						.setThumbnail(data.avatar_url)
+						.setFields(
+							{
+								name: 'Username',
+								value: data.login
+							},
+							{
+								name: 'Name',
+								value: data.name ?? 'Not Provided'
+							},
+							{
+								name: 'Bio',
+								value: data.bio ?? 'Not Provided'
+							},
+							{
+								name: 'Followers',
+								value: data.followers.toString()
+							},
+							{
+								name: 'Following',
+								value: data.following.toString()
+							},
+							{
+								name: 'Join Date',
+								value: new Date(data.created_at).toString()
+							},
+							{
+								name: 'Public Repositories',
+								value: data.public_repos.toString()
+							}
+						)
+				]
+			});
+	}
 };
 
 export const execute = async (interaction: ChatInputCommandInteraction) =>
