42f74ed370a2c1194eeb1e362a40e8aae0f7c744
Akhil Pillai <gh@akpi.is-a.dev>
2023-09-09 20:14:39 -0700

    save

--- scripts/buildCommands.ts
+++ scripts/buildCommands.ts
@@ -1,16 +1,13 @@
+/* eslint-disable no-console */
 import 'dotenv/config';
-import { REST, Routes, SlashCommandBuilder } from 'discord.js';
-import { dirname, default as path } from 'path';
+import { commandsPath, registerCommands } from './registerCommands';
 import { argv } from 'process';
-import { clientId } from '../src/config';
-import { fileURLToPath } from 'url';
 import { readdir } from 'fs/promises';
+
 argv.shift();
 argv.shift();
-const thisdirname = dirname(fileURLToPath(import.meta.url));
-const commands = [];
-const commandsPath = path.join(thisdirname, 'commands');
-let commandFiles;
+
+let commandFiles: string[];
 if (argv.length == 0) {
 	commandFiles = (await readdir(commandsPath)).filter((file) =>
 		file.endsWith('.ts')
@@ -21,30 +18,12 @@ if (argv.length == 0) {
 	);
 }
 if (commandFiles.length == 0) {
-	// eslint-disable-next-line no-console
 	console.log('No commands found');
 	process.exit(1);
 }
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command: {
-		data: SlashCommandBuilder;
-		// eslint-disable-next-line no-unused-vars
-		execute?: (interaction: unknown) => Promise<void>;
-	} = await import(filePath);
-	commands.push(command.data.toJSON());
-}
-if (!process.env.DISCORD_TOKEN) {
-	// eslint-disable-next-line no-console
-	console.log('No token found');
-	process.exit(1);
-}
-// eslint-disable-next-line no-console
-console.log('Registering commands...');
-const data = await new REST()
-	.setToken(process.env.DISCORD_TOKEN as string)
-	.put(Routes.applicationCommands(clientId), {
-		body: commands
-	});
-// eslint-disable-next-line no-console
-console.log(data);
+
+console.log(`Registering ${commandFiles.length} commands...`);
+
+console.log(
+	await registerCommands(process.env.DISCORD_TOKEN as string, commandFiles)
+);
---
+++ scripts/registerCommands.ts
@@ -0,0 +1,43 @@
+import { BaseInteraction, Routes, SlashCommandBuilder } from 'discord.js';
+import { dirname, join } from 'path';
+import { REST } from '@discordjs/rest';
+import { clientId } from '../src/config';
+import { fileURLToPath } from 'url';
+import { readdir } from 'fs/promises';
+
+export const commandsPath = join(
+	dirname(fileURLToPath(import.meta.url)),
+	'..',
+	'src',
+	'commands'
+);
+
+export async function registerCommands(token: string, commandFiles?: string[]) {
+	return new Promise(async (resolve, reject) => {
+		// eslint-disable-next-line no-param-reassign
+		commandFiles =
+			commandFiles ??
+			(await readdir(commandsPath)).filter((file) => file.endsWith('.ts'));
+		const commands = [];
+		for (const file of commandFiles)
+			commands.push(
+				(
+					(await import(join(commandsPath, file))) as {
+						data: SlashCommandBuilder;
+						// eslint-disable-next-line no-unused-vars
+						execute?: (interaction: BaseInteraction) => Promise<void>;
+					}
+				).data.toJSON()
+			);
+		let data: unknown;
+		const rest = new REST().setToken(token);
+		try {
+			data = await rest.put(Routes.applicationCommands(clientId), {
+				body: commands
+			});
+		} catch (e) {
+			reject(e);
+		}
+		resolve(data);
+	});
+}
