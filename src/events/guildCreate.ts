9c634f4a4ff67bf7a3fddfa27523f5320f2f53e1
akpi816218 <816218@seq.org>
2023-09-15 11:48:49 -0700

    Update join message

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -1,4 +1,10 @@
-import { EmbedBuilder, Events, Guild, inlineCode } from 'discord.js';
+import {
+	EmbedBuilder,
+	Events,
+	Guild,
+	inlineCode,
+	underscore
+} from 'discord.js';
 
 export const name = Events.GuildCreate;
 export const once = false;
@@ -12,7 +18,11 @@ export const execute = async (guild: Guild) => {
 					.setDescription(
 						`DisCog is a general purpose Discord bot. Use ${inlineCode(
 							'/coghelp'
-						)} or view my profile to find out what I can do!`
+						)} or view my profile to find out what I can do!\n${underscore(
+							`Admins, be sure to check out the ${inlineCode(
+								'/admin'
+							)} command! This slash command is already restricted to admins, so you don't need to worry about that.`
+						)}`
 					)
 					.setTimestamp()
 			]
