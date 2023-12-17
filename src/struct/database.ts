34ac7ef53b230da1a227840582a4872103358746
akpi816218 <gh@akpi.is-a.dev>
2023-12-16 19:14:29 -0800

    fix: formatting

--- src/struct/database.ts
+++ src/struct/database.ts
@@ -5,73 +5,75 @@ export interface BaseGuildConfig {
 		| {
 				enabled: true;
 				channel: Snowflake;
-		  }
+			}
 		| {
 				enabled?: false | null;
 				channel?: null;
-		  };
+			};
 	birthdays?:
 		| {
 				enabled: true;
 				channel: Snowflake;
-		  }
+			}
 		| {
 				enabled?: false | null;
 				channel?: null;
-		  };
+			};
 	greetings?:
 		| {
 				channel: Snowflake;
 				goodbyeEnabled?: boolean;
 				welcomeEnabled: true;
-		  }
+			}
 		| {
 				channel: Snowflake;
 				goodbyeEnabled: true;
 				welcomeEnabled?: boolean;
-		  }
+			}
 		| {
 				channel?: null;
 				goodbyeEnabled?: false;
 				welcomeEnabled?: false;
-		  };
+			};
 	systemchannel?: Snowflake | null;
 }
 
-export interface PopulatedGuildConfig extends BaseGuildConfig {
-	auditlog:
-		| {
-				enabled: true;
-				channel: Snowflake;
-		  }
-		| {
-				enabled: false;
-				channel: null;
-		  };
-	birthdays:
-		| {
-				enabled: true;
-				channel: Snowflake;
-		  }
-		| {
-				enabled: false;
-				channel: null;
-		  };
-	greetings:
-		| {
-				channel: Snowflake;
-				goodbyeEnabled: boolean;
-				welcomeEnabled: true;
-		  }
-		| {
-				channel: Snowflake;
-				goodbyeEnabled: true;
-				welcomeEnabled: boolean;
-		  }
-		| {
-				channel: null;
-				goodbyeEnabled: false;
-				welcomeEnabled: false;
-		  };
-	systemchannel: Snowflake | null;
-}
+/**
+	export interface PopulatedGuildConfig extends BaseGuildConfig {
+		auditlog:
+			| {
+					enabled: true;
+					channel: Snowflake;
+				}
+			| {
+					enabled: false;
+					channel: null;
+				};
+		birthdays:
+			| {
+					enabled: true;
+					channel: Snowflake;
+				}
+			| {
+					enabled: false;
+					channel: null;
+				};
+		greetings:
+			| {
+					channel: Snowflake;
+					goodbyeEnabled: boolean;
+					welcomeEnabled: true;
+				}
+			| {
+					channel: Snowflake;
+					goodbyeEnabled: true;
+					welcomeEnabled: boolean;
+				}
+			| {
+					channel: null;
+					goodbyeEnabled: false;
+					welcomeEnabled: false;
+				};
+		systemchannel: Snowflake | null;
+	}
+*/
